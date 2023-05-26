9209e3f5f3db63d435004b598db868b05a3fe00c
hiokimiyu <106291672+hiokimiyu@users.noreply.github.com>
2023-05-26 16:37:21 +0900

    [add]アイテムスクリプト追加、アイテムプレハブの作成、スポナーをプレハブ化

---
+++ Assets/Hioki/Scripts/Item.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField, Tooltip("ハンマーだったらtrue")]
+    bool _ishammer = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //PlayerItemがぶつかったGameObjectについていた時
+        if (collision.gameObject.TryGetComponent<PlayerItem>(out PlayerItem playerItem))
+        {
+            //ハンマーの時
+            if (_ishammer)
+            {
+                playerItem.Hammer();
+            }
+            //スターの時
+            else
+            {
+                playerItem.Star();
+            }
+        }
+    }
+}
