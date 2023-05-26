29abacbe1188c359419e87dda05201adff293a45
KakinumaYuzuki <V.proYuzu23@gmail.com>
2023-05-26 18:18:46 +0900

    [update]Playerの動き

--- Assets/Kakinuma/Script/Jump.cs
+++ Assets/Kakinuma/Script/Jump.cs
@@ -7,7 +7,7 @@ public class Jump : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] bool _canJamp = false;
     [SerializeField] float _jumpPower = 0;
-    [SerializeField] public float jumpTime = 0;
+    [SerializeField] public float _jumpTime = 0;
     Rigidbody2D rb = default;
     void Start()
     {
@@ -17,24 +17,26 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_canJamp)
+        if (_canJamp)　//ジャンプ
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 rb.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
-                //rb.AddForce(new Vector2.0,_jumpPower, ForceMode2D.Impulse);
+                _jumpTime++;
+                _canJamp = false;
             }
 
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //ジャンプ
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Slope")
         {
             _canJamp = true;
+            _jumpTime = 0;
         }
-        else
+        if (_jumpTime > 0)
         {
             _canJamp = false;
         }
--- Assets/Kakinuma/Script/PlayerController.cs
+++ Assets/Kakinuma/Script/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     public float speed; //移動速度(インスペクターから変更可)
     private Rigidbody2D rb = null;
+    [SerializeField] GameObject _footPoint = default;
+    [SerializeField, Tooltip("rayの長さ")] float _rayLength = 0f;
+
+    public float _ladderSpeed; //移動速度(インスペクターから変更可)
+    private bool isLadder = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //Rigidbody2Dのインスタンスを取得
@@ -16,8 +21,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horizontalKey = Input.GetAxis("Horizontal");
-        //float verticalKey = Input.GetAxis("Vertical");
+        float verticalKey = Input.GetAxisRaw("Vertical");
         float xSpeed = 0;
+        float ySpeed = 0;
         if (horizontalKey > 0) //右移動速度
         {
             //transform.localScale = new Vector3(1, 1, 1);
@@ -27,7 +33,40 @@ public class PlayerController : MonoBehaviour
         {
             xSpeed = -speed;
         }
-        rb.velocity = new Vector2(xSpeed, rb.velocity.y); //速度最終結果(yはGravity Scale)
 
+        if (isLadder)
+        {
+            if (verticalKey > 0)
+            {
+                ySpeed = verticalKey * 0.15f;
+            }
+        }
+        //RaycastHit2D raycastHit2D = Physics2D.Raycast(_footPoint.transform.position, Vector2.right, _rayLength);
+        //Vector2 _playerUp = raycastHit2D.normal;
+        //if(raycastHit2D.collider.gameObject.CompareTag("Slope"))
+        //{
+        //    var temp = Vector3.ProjectOnPlane(Vector2.right, -_playerUp);
+        //    var _direction = new Vector2(temp.x, temp.y);
+        //    rb.velocity = _direction * xSpeed + Vector2.up * rb.velocity.y;
+        //}
+        //else
+        //{
+        //    rb.velocity = new Vector2(xSpeed, rb.velocity.y); //速度最終結果(yはGravity Scale)
+        //}
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y+ySpeed); //速度最終結果(yはGravity Scale)
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "LadderControll")
+        {
+            isLadder = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "LadderControll")
+        {
+            isLadder = false;
+        }
     }
 }
