4fe8aca961a6ddc53803622cce1241fb18e660e0
Ray <rei.piyo.55687@gmail.com>
2023-06-27 15:39:15 +0900

    一次保存

---
+++ Assets/Yuki/GoGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoGame : MonoBehaviour
+{
+    public void GoGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
