ffa0b178ae6df6e3d5447d309d1187bfb4cb1079
Ray <rei.piyo.55687@gmail.com>
2023-07-12 20:15:01 +0900

    [fix]Resultシーンに数値が引き継がれていないのを修正

--- Assets/Yuki/Script/LoadResult.cs
+++ Assets/Yuki/Script/LoadResult.cs
@@ -7,11 +7,15 @@ public class LoadResult : MonoBehaviour
     [SerializeField, Tooltip("ボーナスに乗算する値")] float _bonusAdd = 0f;
     [SerializeField, Tooltip("ボーナステキスト")] Text _bonusText = default;
     [SerializeField] GameManager _gm = default;
+    [SerializeField, Tooltip("ボーナステキスト")] Text _timerText = default;
+
 
     void Start()
     {
         _bonusScore = _gm.LeftTime * _bonusAdd + _gm.Score;
         _bonusText.text = "ボーナス" + _bonusScore.ToString("00000");
+        _timerText.text = "Time:" + _gm.LeftTime.ToString("00000");
+
     }
 
 }
